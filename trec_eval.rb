d2f4b9c30182fb8b24a96d6ff8fa9241d2e32b77
Benjamin Piwowarski <benjamin@bpiwowar.net>
2016-10-04 13:20:32 +0200

    Added trec_eval latest as HEAD

--- trec_eval.rb
+++ trec_eval.rb
@@ -5,10 +5,9 @@ class TrecEval < Formula
   url "http://trec.nist.gov/trec_eval/trec_eval.8.1.tar.gz"
   sha256 "38147c4a0fa5d13b09092b0f8dd55142902ae2a4bc6e34db03fadb78f28f612e"
 
+  head "http://trec.nist.gov/trec_eval/trec_eval_latest.tar.gz", :using => :curl
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
     # Remove unrecognized options if warned by configure
     system "mkdir", "#{prefix}/bin"
     system "make", "install", "BIN=#{prefix}/bin"
